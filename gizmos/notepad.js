74a0eb0d3cfc679cce0d2eac3990a66b1d73d2dc
Pandademic <74566464+Pandademic@users.noreply.github.com>
2021-07-02 13:48:22 -0400

    Create notepad.js

---
+++ gizmos/notepad.js
@@ -0,0 +1,8 @@
+var OpenApp;
+function Note(){
+    document.getElementById("NotepadApp").style.display="inline-block";
+    OpenApp="NotepadApp"
+}
+function Close(){
+    document.getElementById(OpenApp).style.display="none"
+}
